0f8e87a636f822ed88d84e6d2ecce7b2b4903ea5
AndreFilho0 <filhoandre534@gmail.com>
2023-11-03 23:12:51 -0300

    add slides

--- js/slick.js
+++
@@ -1,5 +0,0 @@
-$('.menu-meio').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
---
+++ js/slide.js
@@ -0,0 +1,9 @@
+let cont=1;
+setInterval(function(){
+  document.getElementById('slide' + cont).checked = true;
+  cont++;
+  if( cont>5 ){
+    cont = 1;
+  }
+
+},5000)
